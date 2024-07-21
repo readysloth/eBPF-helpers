e3367128482b8ec848326c6d7b840a871d0893ab
readysloth <readysloth@protonmail.com>
2024-07-21 18:55:29 +0300

    feat: Added more variative test cases

--- tests/unit-tests.c
+++ tests/unit-tests.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stddef.h>
 
 #include <ebpf-helpers.h>
 
@@ -14,23 +15,70 @@ import cog
 
 utest_case_fmt = '''
 UTEST({namespace}, {test}) {{
-  char bytes_to_conv[] = {{ {bytes_to_conv} }};
+  uint8_t bytes_to_conv[] = {{ {bytes_to_conv} }};
   char base64_str_correct[] = "{base64_str_correct}";
 
-  char base64_str[sizeof(bytes_to_conv) * 4 / 3] = {{0}};
-  bytes_to_base64(bytes_to_conv, strlen(bytes_to_conv), base64_str, sizeof(base64_str));
+  char base64_str[{base64_str_size}] = {{0}};
+  size_t written = bytes_to_base64(bytes_to_conv, sizeof(bytes_to_conv), base64_str, sizeof(base64_str));
+
+  if (sizeof(bytes_to_conv) == 0) {{
+    ASSERT_EQ(written, 0);
+    return;
+  }}
+  if (sizeof(base64_str) < sizeof(base64_str_correct)) {{
+    ASSERT_EQ(written, 0);
+    return;
+  }}
+  ASSERT_EQ(written, sizeof(base64_str_correct));
   ASSERT_STREQ(base64_str_correct, base64_str);
 }}
 '''
 
-for i in range(16):
-  generated_bytes = random.randbytes(random.randrange(1, 1024))
+for i in range(100):
+  generated_bytes = random.randbytes(random.randrange(0, 4))
+  b64_encoded_bytes = base64.b64encode(generated_bytes).decode()
+  out_buf_size = len(b64_encoded_bytes) + random.choice([-1, +1]);
+  if out_buf_size < 0:
+    out_buf_size = 0
   cog.outl(
     utest_case_fmt.format(
       namespace='base64',
-      test=i,
+      test='less_than_4_' + str(i),
       bytes_to_conv=', '.join([hex(b) for b in generated_bytes]),
-      base64_str_correct=base64.b64encode(generated_bytes).decode(),
+      base64_str_correct=b64_encoded_bytes,
+      base64_str_size=out_buf_size
+    )
+  )
+
+for i in range(100):
+  generated_bytes = random.randbytes(random.randrange(4, 100))
+  b64_encoded_bytes = base64.b64encode(generated_bytes).decode()
+  out_buf_size = len(b64_encoded_bytes) + random.choice([-1, +1]);
+  if out_buf_size < 0:
+    out_buf_size = 0
+  cog.outl(
+    utest_case_fmt.format(
+      namespace='base64',
+      test='small_buf_' + str(i),
+      bytes_to_conv=', '.join([hex(b) for b in generated_bytes]),
+      base64_str_correct=b64_encoded_bytes,
+      base64_str_size=out_buf_size
+    )
+  )
+
+for i in range(100):
+  generated_bytes = random.randbytes(random.randrange(1024, 4096))
+  b64_encoded_bytes = base64.b64encode(generated_bytes).decode()
+  out_buf_size = len(b64_encoded_bytes) + random.choice([-1, +1]);
+  if out_buf_size < 0:
+    out_buf_size = 0
+  cog.outl(
+    utest_case_fmt.format(
+      namespace='base64',
+      test='big_buf_' + str(i),
+      bytes_to_conv=', '.join([hex(b) for b in generated_bytes]),
+      base64_str_correct=b64_encoded_bytes,
+      base64_str_size=out_buf_size
     )
   )
 
